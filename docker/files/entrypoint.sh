c3685d1cb2bf7fa5ec855d60a1da4ec7d2ac4073
Jakub Pavlik <pavlk.jakub@gmail.com>
2016-10-12 18:07:12 +0200

    missing dependency and cassandra.yaml

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -2,20 +2,20 @@
 
 [ -z "$1" ] && (echo "Usage: $0 <component>" 1>&2; exit 1)
 
-if [ "$2" == "check" ]; then
+if [ "$2" = "check" ]; then
     su nova -s /bin/sh -c "/usr/bin/opencontrail-check"
     exit $?
 fi
 
 # Replace env variables for database config files
-if [ "$1" == "database" ]; then
+if [ "$1" = "database" ]; then
     echo "Replace env variables"
     cat /usr/share/kafka/config/server.properties | envsubst > /var/lib/contrail/server.properties
     cat /etc/cassandra/cassandra.yaml | envsubst > /var/lib/contrail/cassandra.yaml
 fi
 
 # Replace env variables for control config file
-if [ "$1" == "control" ]; then
+if [ "$1" = "control" ]; then
     echo "Replace env variables"
     cat /etc/contrail/contrail-control.conf | envsubst > /var/lib/contrail/contrail-control.conf
     cat /etc/contrail/contrail-dns.conf | envsubst > /var/lib/contrail/contrail-dns.conf
--- docker/opencontrail-database.Dockerfile
+++ docker/opencontrail-database.Dockerfile
@@ -1,9 +1,9 @@
 FROM opencontrail/opencontrail-base
 
 RUN apt-get update && \
-    DEBIAN_FRONTEND=nointeractive apt-get install -y cassandra contrail-database openjdk-7-jre-headless && \
+    DEBIAN_FRONTEND=nointeractive apt-get install -y cassandra python-yaml contrail-database openjdk-7-jre-headless && \
     sed -i "s,/usr/share/kafka/config/,/var/lib/contrail/,g" /etc/contrail/supervisord_database_files/kafka.ini && \
-    sed -i 's,command=cassandra,command=cassandra\ \-D\ cassandra.config=/var/lib/contrail,g' /etc/contrail/supervisord_database.conf \
+    sed -i 's,command=cassandra,command=cassandra\ \-D\ cassandra.config=/var/lib/contrail/cassandra.yaml,g' /etc/contrail/supervisord_database.conf \
     && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 ENTRYPOINT ["/entrypoint.sh"]
